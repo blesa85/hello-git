e2c6818d9d14b62d6f12868cb2f2748c9545c1ec
Fernando Blesa <fernandoblesagarcia@gmail.com>
2025-03-03 19:31:44 +0100

    Update hello3.java
    Commit desde github desktop

--- hello3.java
+++ hello3.java
@@ -1,5 +1,5 @@
 public class hello3 {
     public static void main(String[] args) {
-        System.out.println("Hello V3, now!");
+        System.out.println("Hello V3, desde github desktop!");
     }
 }
